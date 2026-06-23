002dbeb324b47f05bca0607d033b3790800df884
technoweit77 <technoweit77@gmail.com>
2026-06-23 11:59:37 +0530

    done

--- JS/hello.js
+++ JS/hello.js
@@ -6,3 +6,39 @@ let b = 43
 console.log(a)
 let answer = a + b
 console.log(answer)
+
+// console.log(a++)  //123
+// console.log(++a) //124
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(a--) //123
+// console.log(--a) //122
+
+
+console.log(a >= b)
+console.log(a >= b && b == 0)
+
+//conditional Statements
+if (a >= b) {
+    //if block
+
+} else if () {
+    //else block
+}
+else if () {
+    //else block
+}
+else if () {
+    //else block
+} else {
+    
+}
